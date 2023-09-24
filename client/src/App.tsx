4a8d77e43571b3ea6ef05cdefe08b8f27723ae23
KeoFoxy <arasdiel68@gmail.com>
2023-09-24 19:27:58 +0300

    App tsx logic done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import { logo } from "./assets"
@@ -16,8 +15,8 @@ const App = () => {
         </header>
         <main className={"sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]"}>
             <Routes>
-                <Route />
-                <Route />
+                <Route path='/' element={<Home />}/>
+                <Route path='/create-post' element={<CreatePost />}/>
             </Routes>
         </main>
     </BrowserRouter>
